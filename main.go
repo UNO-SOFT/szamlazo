a895f9064b46e7c3a0c2b9d039abc15c066a62b0
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-11-18 20:02:46 +0100

    main: serve .

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
 //go:generate go get -v github.com/oskca/gopherjs-vue
 //go:generate go generate ./basic
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+	addr := flag.Arg(0)
+	if addr == "" {
+		addr = ":8080"
+	}
+	log.Println("Listening on " + addr)
+	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir("."))))
 }
